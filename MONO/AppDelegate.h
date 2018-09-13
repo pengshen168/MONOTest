848d3601c544661296ea796952867bce84713584
peng139 <1067385702@qq.com>
2018-09-13 17:26:48 +0800

    Initial Commit

---
+++ MONO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MONO
+//
+//  Created by 万启鹏 on 2018/9/13.
+//  Copyright © 2018年 万启鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MONO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MONO
+//
+//  Created by 万启鹏 on 2018/9/13.
+//  Copyright © 2018年 万启鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
